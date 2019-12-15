147c28888fb737d5aaa18f7ad60646b6a2b86a9a
NIKHIL KUMAR GAUTAM <44256345+nikhilkrgautam@users.noreply.github.com>
2019-12-16 02:27:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-
 app.use(express.static('public'))
 
 app.use('/static', express.static('public'))
